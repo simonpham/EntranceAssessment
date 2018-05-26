107a88997187552ad948fbaf2ab7b74393d37709
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 17:59:06 +0700

    Add loadUrl()

--- app/src/main/java/com/github/simonpham/hasbrain/ea/ui/browser/BrowserActivity.kt
+++ app/src/main/java/com/github/simonpham/hasbrain/ea/ui/browser/BrowserActivity.kt
@@ -1,13 +1,28 @@
 package com.github.simonpham.hasbrain.ea.ui.browser
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.webkit.WebViewClient
 import com.github.simonpham.hasbrain.ea.R
+import kotlinx.android.synthetic.main.activity_browser.*
 
 class BrowserActivity : AppCompatActivity() {
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browser)
+
+        // enable Javascript
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        val url = intent.getStringExtra("MainActivity.ARTICLE_URL")
+        loadUrl(url)
+    }
+
+    private fun loadUrl(url: String) {
+        if (webView != null) webView.loadUrl(url)
     }
 }
