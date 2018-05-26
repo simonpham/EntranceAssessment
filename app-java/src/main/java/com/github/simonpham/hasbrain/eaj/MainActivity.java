f783a6a71dbf6da979d886ef11c1a32242108aff
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 19:56:07 +0700

    Add old resources

--- app-java/src/main/java/com/github/simonpham/hasbrain/eaj/MainActivity.java
+++ app-java/src/main/java/com/github/simonpham/hasbrain/eaj/MainActivity.java
@@ -4,12 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Toolbar toolbar;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +18,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.nav_explorer:
+                    toolbar.setTitle(R.string.title_explore);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.nav_saved:
+                    toolbar.setTitle(R.string.title_saved);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.nav_me:
+                    toolbar.setTitle(R.string.title_me);
+                    return true;
+                case R.id.nav_quest:
+                    toolbar.setTitle(R.string.title_quest);
+                    return true;
+                case R.id.nav_inbox:
+                    toolbar.setTitle(R.string.title_inbox);
                     return true;
             }
             return false;
@@ -36,9 +43,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        bindView();
+        setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void bindView() {
+        toolbar = findViewById(R.id.toolbar);
     }
 
 }
