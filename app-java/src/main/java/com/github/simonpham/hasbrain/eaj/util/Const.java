f0f680b60223bfc344fe95ef5b97982380aa1ed7
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 21:26:49 +0700

    Add TimeAgo.java

--- app-java/src/main/java/com/github/simonpham/hasbrain/eaj/util/Const.java
+++ app-java/src/main/java/com/github/simonpham/hasbrain/eaj/util/Const.java
@@ -8,4 +8,11 @@ package com.github.simonpham.hasbrain.eaj.util;
 public class Const {
 
     public static final int NO_IMAGE = -1;
+
+    public static final long ONE_MINUTE = 60;
+    public static final long ONE_HOUR = 3600;
+    public static final long ONE_DAY = 86400;
+    public static final long ONE_WEEK = 604800;
+    public static final long ONE_MONTH = 2629743;
+    public static final long ONE_YEAR = 31556926;
 }
---
+++ app-java/src/main/java/com/github/simonpham/hasbrain/eaj/util/TimeAgo.java
@@ -0,0 +1,64 @@
+package com.github.simonpham.hasbrain.eaj.util;
+
+import android.annotation.SuppressLint;
+
+import static com.github.simonpham.hasbrain.eaj.util.Const.ONE_DAY;
+import static com.github.simonpham.hasbrain.eaj.util.Const.ONE_HOUR;
+import static com.github.simonpham.hasbrain.eaj.util.Const.ONE_MINUTE;
+import static com.github.simonpham.hasbrain.eaj.util.Const.ONE_MONTH;
+
+/**
+ * Created by Simon Pham on 5/26/18.
+ * Email: simonpham.dn@gmail.com
+ */
+public class TimeAgo {
+
+    @SuppressLint("SimpleDateFormat")
+    public static String getDisplayTime(long time) {
+        String displayTime = getTimeAgo(time);
+        if (!(displayTime.startsWith("-") || displayTime.startsWith("0"))) {
+            return displayTime;
+        }
+
+        return new java.text.SimpleDateFormat("HH:mm:ss dd/MM/yyyy ").format(new java.util.Date(time * 1000));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private static String getTimeAgo(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        long elapsedTime = currentTime - time;
+
+        if (elapsedTime >= 0 && elapsedTime < ONE_MINUTE) {
+            return "A few seconds ago";
+        } else if (elapsedTime < ONE_HOUR) {
+            return toMinutes(time) + "m ago";
+        } else if (elapsedTime < ONE_DAY) {
+            return toHours(time) + "h ago";
+        } else if (elapsedTime < ONE_MONTH) {
+            return toDays(time) + "d ago";
+        }
+
+        return new java.text.SimpleDateFormat("HH:mm:ss dd/MM/yyyy ").format(new java.util.Date(time * 1000));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private static int toMinutes(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("m").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("m").format(new java.util.Date(time * 1000)));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private static int toHours(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("H").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("H").format(new java.util.Date(time * 1000)));
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private static int toDays(long time) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return Integer.parseInt(new java.text.SimpleDateFormat("D").format(new java.util.Date(currentTime * 1000)))
+                - Integer.parseInt(new java.text.SimpleDateFormat("D").format(new java.util.Date(time * 1000)));
+    }
+}
