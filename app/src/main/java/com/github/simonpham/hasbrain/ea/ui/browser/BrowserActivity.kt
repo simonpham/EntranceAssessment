b95d454af007b5115855b5cc92369ae0e93b8ea0
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 17:45:13 +0700

    Create browser activity

---
+++ app/src/main/java/com/github/simonpham/hasbrain/ea/ui/browser/BrowserActivity.kt
@@ -0,0 +1,13 @@
+package com.github.simonpham.hasbrain.ea.ui.browser
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.github.simonpham.hasbrain.ea.R
+
+class BrowserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browser)
+    }
+}
