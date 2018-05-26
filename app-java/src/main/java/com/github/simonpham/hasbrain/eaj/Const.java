3f5da67c5d55ff6baa5e78857aec5b63b237a098
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 20:03:14 +0700

    Add Const.java

---
+++ app-java/src/main/java/com/github/simonpham/hasbrain/eaj/Const.java
@@ -0,0 +1,11 @@
+package com.github.simonpham.hasbrain.eaj;
+
+/**
+ * Created by Simon Pham on 5/26/18.
+ * Email: simonpham.dn@gmail.com
+ */
+
+public class Const {
+
+    public int NO_IMAGE = -1;
+}
