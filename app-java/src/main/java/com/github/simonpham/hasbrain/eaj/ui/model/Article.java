34e31158a94439378c08f5dccd5d689bec65f929
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 21:00:44 +0700

    Add Article.java constructor

--- app-java/src/main/java/com/github/simonpham/hasbrain/eaj/ui/model/Article.java
+++ app-java/src/main/java/com/github/simonpham/hasbrain/eaj/ui/model/Article.java
@@ -1,5 +1,7 @@
 package com.github.simonpham.hasbrain.eaj.ui.model;
 
+import static com.github.simonpham.hasbrain.eaj.Const.NO_IMAGE;
+
 /**
  * Created by Simon Pham on 5/26/18.
  * Email: simonpham.dn@gmail.com
@@ -9,6 +11,24 @@ public class Article {
     private String title;
     private int idImage;
     private String articleUrl;
+
+    public Article(String title, int idImage, String articleUrl) {
+        this.title = title;
+        this.idImage = idImage;
+        this.articleUrl = articleUrl;
+    }
+
+    public Article(String title, String articleUrl) {
+        this.title = title;
+        this.idImage = NO_IMAGE;
+        this.articleUrl = articleUrl;
+    }
+
+    public Article(String title) {
+        this.title = title;
+        this.idImage = NO_IMAGE;
+        this.articleUrl = "about:blank";
+    }
 
     public String getTitle() {
         return title;
