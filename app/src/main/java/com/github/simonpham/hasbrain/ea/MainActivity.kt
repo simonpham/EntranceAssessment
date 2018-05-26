d852fcccd28f9977caef8a3b506efd53e5bcf6d9
Simon Pham <simonpham.dn@gmail.com>
2018-05-26 11:33:40 +0700

    Setup action bar

--- app/src/main/java/com/github/simonpham/hasbrain/ea/MainActivity.kt
+++ app/src/main/java/com/github/simonpham/hasbrain/ea/MainActivity.kt
@@ -3,6 +3,7 @@ package com.github.simonpham.hasbrain.ea
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,23 +11,23 @@ class MainActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_explorer -> {
-                message.setText(R.string.title_explore)
+                toolbar.title = getString(R.string.title_explore)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_saved -> {
-                message.setText(R.string.title_saved)
+                toolbar.title = getString(R.string.title_saved)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_me -> {
-                message.setText(R.string.title_me)
+                toolbar.title = getString(R.string.title_me)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_quest -> {
-                message.setText(R.string.title_quest)
+                toolbar.title = getString(R.string.title_quest)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_inbox -> {
-                message.setText(R.string.title_inbox)
+                toolbar.title = getString(R.string.title_inbox)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -36,7 +37,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+//        recyclerView.adapter =
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.action_bar, menu)
+        return true
     }
 }
